beab042bc5d06a7f25567b6a4c36b13c3ab8b391
Andrei <ag-geck@yandex.ru>
2019-01-05 17:03:55 -0700

    deleted php errors logging

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
-ini_set('error_log', "phperrors.log"); 
-error_reporting(E_ALL);
-
-mail('andrei@insightdesigns.com', 'test', 'test');
 
 header("Access-Control-Allow-Origin: *");
 /**
