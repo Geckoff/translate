c81896ccc893ccc22b0e8dd00a28d7eeb65adb65
Andrei <ag-geck@yandex.ru>
2019-01-06 16:22:47 -0700

    updated iframes

--- wp-content/themes/translate/js/scripts.js
+++ wp-content/themes/translate/js/scripts.js
@@ -1,9 +1,8 @@
 (function($){
-    $(document).on('ready', function(){
+    $(document).on('ready', function(){ 
         var headerIframe = window.parent.document.querySelector('.header-iframe');
         if (headerIframe) {
             document.getElementById('mobile-navigation-btn').addEventListener('click', function(e){
-                console.log($('.header-iframe'));
                 $(headerIframe).toggleClass('iframe-menu-enabled');  
             });
         }
--- wp-content/themes/translate/page-application.php
+++ wp-content/themes/translate/page-application.php
@@ -8,8 +8,21 @@
     window.userData = [
         '<?=admin_url('admin-ajax.php'); ?>',
         '<?=wp_create_nonce('gecktranslate'); ?>',
-        <?=get_current_user_id(); ?>    
-    ]
+        <?=get_current_user_id(); ?>,
+        '<?=get_home_url(); ?>'
+    ];
+
+    
+    document.addEventListener('DOMContentLoaded', function(){
+        var headerIframe = document.querySelector('.header-iframe');
+            footerIframe = document.querySelector('.footer-iframe');
+        headerIframe.setAttribute('src', window.userData[3] + '/profile-bar/');    
+        footerIframe.setAttribute('src', window.userData[3] + '/footer-page/'); 
+        headerIframe.contentWindow.location = window.userData[3] + '/profile-bar/';
+        footerIframe.contentWindow.location = window.userData[3] + '/footer-page/';
+    }, false);
+          
+    
 </script>
 <?php 
 /*
